b8ce1baa885b3789501155d8e0cfdc8e8831f66f
Developer sam <gitongas684@gmail.com>
2023-03-24 09:09:20 +0300

    Initial commit

---
+++ Bank-microservice-user-registration/src/main/java/com/devsam/Bankmicroserviceuserregistration/BankMicroserviceUserRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.devsam.Bankmicroserviceuserregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankMicroserviceUserRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankMicroserviceUserRegistrationApplication.class, args);
+	}
+
+}
---
+++ Bank-microservice-user-registration/src/test/java/com/devsam/Bankmicroserviceuserregistration/BankMicroserviceUserRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsam.Bankmicroserviceuserregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankMicroserviceUserRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
